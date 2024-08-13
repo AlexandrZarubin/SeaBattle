1d5a7fd9205af97db23887ecdba51ce9dfd6761e
Alexander Zarubin <whitesashka@gmail.com>
2024-08-13 16:25:02 +0500

    1

--- SeaBattle/Map.cpp
+++ SeaBattle/Map.cpp
@@ -2,34 +2,6 @@
 
 void Map::Print()
 {
-    //std::cout << std::string(20, ' ') << "ПОЛЕ PC" << std::string(43, ' ') << "ПОЛЕ PC\n";
-    //std::cout << " %   A   B   C   D   E   F   G   H   I   J" << std::string(5, ' ') << "   %   A   B   C   D   E   F   G   H   I   J\n";;
-    //for (int i = 1; i < 11 ; i++)//строки
-    //{
-
-    //    std::cout <<Pole1<<std::string(5,' ') <<Pole1<< "\n";// Линия разделителя
-    //    std::cout << std::setw(2) << i << " ";//setw для цифры 10
-    //    for (int j = 1; j < 12; j++)// столбцы
-    //    {
-    //        std::cout << "| " << "  ";
-    //        if (j == 10) std::cout << "|";
-    //        if (j == 10) 
-    //        {
-    //            std::cout << std::string(5, ' ');// Пробел между полями
-    //            // Номер строки и содержимое поля ПК
-    //            std::cout << std::setw(2) << i << " ";
-    //            for (int j = 1; j < 11; j++)
-    //            {
-    //                std::cout << "| " << "  ";
-    //               
-    //            }
-    //        }
-    //    }
-    //    std::cout << std::endl;
-    //   
-
-    //}
-    //    std::cout <<Pole1<<std::string(5,' ') <<Pole1<< "\n";
     std::cout << std::string(20, ' ') << "ПОЛЕ ИГРОКА" << std::string(43, ' ') << "ПОЛЕ ПК\n";
     std::cout << ABC << std::string(12, ' ') <<ABC<<"\n";
 
